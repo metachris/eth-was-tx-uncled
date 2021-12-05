55f2787f0c9e1ac2c8feb1081de56ff0724d02c6
Chris Hager <chris@linuxuser.at>
2021-12-05 14:17:54 +0100

    check uncles of previous blocks too

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"math/big"
 	"os"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 var ethNodeUriPtr = flag.String("eth", os.Getenv("ETH_NODE_URI"), "URL for eth node (eth node, Infura, etc.)")
 var txHashPtr = flag.String("tx", "", "tx hash")
+var debugPtr = flag.Bool("debug", false, "print additional information")
 
 func Perror(err error) {
 	if err != nil {
@@ -47,18 +50,37 @@ func main() {
 	block, err := client.BlockByHash(context.Background(), receipt.BlockHash)
 	Perror(err)
 
-	// TODO: if this block has no uncles, perhaps try a few blocks back?
-	for _, uncle := range block.Uncles() {
-		uncleBlock, err := client.BlockByHash(context.Background(), uncle.Hash())
-		Perror(err)
-
-		for _, uncleTx := range uncleBlock.Transactions() {
-			if tx.Hash() == uncleTx.Hash() {
-				fmt.Printf("Found tx in uncle %s\n", uncle.Hash().Hex())
-				return
-			}
+	// check uncles of included block, and if not found check the previous few blocks for uncles
+	for blocksTried := 0; blocksTried < 6; blocksTried++ {
+		found, blockHash := wasTxFoundInUncles(client, block, tx.Hash())
+		if found {
+			fmt.Printf("Found tx in uncle %s\n", blockHash.Hex())
+			return
 		}
+		prevBlockNumber := big.NewInt(block.Number().Int64() - 1)
+		block, err = client.BlockByNumber(context.Background(), prevBlockNumber)
+		Perror(err)
 	}
 
 	fmt.Printf("Tx %s not found in uncles\n", tx.Hash().Hex())
+}
+
+func wasTxFoundInUncles(client *ethclient.Client, block *types.Block, txHash common.Hash) (found bool, blockHash common.Hash) {
+	if *debugPtr {
+		fmt.Println("checking block", block.Number().Int64())
+	}
+	for _, uncle := range block.Uncles() {
+		uncleBlock, err := client.BlockByHash(context.Background(), uncle.Hash())
+		Perror(err)
+		if *debugPtr {
+			fmt.Println("- checking uncle", uncleBlock.Hash().Hex())
+		}
+
+		for _, uncleTx := range uncleBlock.Transactions() {
+			if txHash == uncleTx.Hash() {
+				return true, uncle.Hash()
+			}
+		}
+	}
+	return false, blockHash
 }
